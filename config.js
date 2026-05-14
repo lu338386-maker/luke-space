51fb1f82a0f83c9ba687066ec56932207369bd2e
lu338386-maker <lu338386@gmail.com>
2026-05-14 20:31:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// Online deployment config (public values only)
-// Fill with your Supabase project settings before production.
-window.SUPABASE_URL = "https://kdcrpriulfosfytrznxi.supabase.co";
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkY3Jwcml1bGZvc2Z5dHJ6bnhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3NTMxMTMsImV4cCI6MjA5NDMyOTExM30.LMrFgLaqF_pa8SuNmtL59M3iHzSHg4QTnE0Fp5-3xTY";
-window.APP_VERSION = window.APP_VERSION || "v0.3.0";
+[object Object],.,[object Object], = ,[object Object],;
+,[object Object],.,[object Object], = ,[object Object],;
+,[object Object],.,[object Object], = ,[object Object],;
